a8d6af110a33ab290c9dbb3a91b15ad2cb57a1fa
jhad7010 <jhad7010@gmail.com>
2016-03-28 22:20:31 -0600

    Adicionando instrucciones iniciales

--- Tarea Modulo2/ViewController.swift
+++ Tarea Modulo2/ViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
+// A los compañeros de clase, favor copiar todo el programa dentro de su Playground para poderlo ejecutar.
 
 // Definición de rango de números a utilizar
 var rango = 1...100
 
 // La variable numero es la que se utilizará para las validaciones dentro del ciclo.
+
 for numero in rango {
     
     // Si es divisible entre 5
