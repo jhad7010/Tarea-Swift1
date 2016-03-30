8a0ef27da2cc3aed82547b202d9e599dde57be66
Horacio Garza <hgx95@hotmail.com>
2016-03-30 10:01:48 -0600

    Some issues, but resolevd
    The rank 1...100 was deprecated since it goes from 1-100 without the 0, I've had deleted the variable 'rango' cause it was unnecessary; also the If conditionals was bad evaluated.

--- Tarea Modulo2/ViewController.swift
+++ Tarea Modulo2/ViewController.swift
@@ -10,25 +10,26 @@ import UIKit
 
 // A los compañeros de clase, favor copiar todo el programa dentro de su Playground para poderlo ejecutar.
 
-// Definición de rango de números a utilizar
-var rango = 1...100
-
 // La variable numero es la que se utilizará para las validaciones dentro del ciclo.
 
-for numero in rango {
+for i in 0...100 {
     
     // Si es divisible entre 5
     if ( numero % 5 ) == 0 {
         print("\(numero), #Bingo!!!")
-    } else if ( numero % 2 ) == 0 { //Revisa si es divisible entre 2
+    } 
+    
+    //Revisa si es divisible entre 2
+    if ( numero % 2 ) == 0 { 
         print("\(numero), #Par!")
-        
     } else {
         print("\(numero), #Impar!") // Al no cumplir las condiciones anteriores se determina que es Impar
         
-    } // Ahora se evalúa el rango del número que esté entre 30 y 40.
+    } 
+    
+    // Ahora se evalúa el rango del número que esté entre 30 y 40.
     if numero >= 30 && numero <= 40 {
-        print("\(numero), #Viva Swift")
+        print("\(numero), #Viva Swift!")
     }
     
     
